25ab3901abec0fb1e46291a983b0a8f2012df66b
biscuitdey <biswashreedey@gmail.com>
2025-05-26 12:52:12 +0530

    Bug fix

--- bri-3/src/bri/zeroKnowledgeProof/zeroKnowledgeProof.module.ts
+++ bri-3/src/bri/zeroKnowledgeProof/zeroKnowledgeProof.module.ts
@@ -3,7 +3,7 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { LoggingModule } from '../../shared/logging/logging.module';
 import { CircuitInputsParserService } from './services/circuit/circuitInputsParser/circuitInputParser.service';
 import { SnarkjsCircuitService } from './services/circuit/snarkjs/snarkjs.service';
-import { GeneralCircuitInputsParserService } from './services/circuit/circuitInputsParser/generalCircuitInputParser.service copy';
+import { GeneralCircuitInputsParserService } from './services/circuit/circuitInputsParser/generalCircuitInputParser.service';
 
 @Module({
   imports: [CqrsModule, LoggingModule],
