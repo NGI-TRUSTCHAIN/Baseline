a45ab5fbe81131acb16387263b23e2e2b6c05326
ognjenkurtic <kurticognjen@gmail.com>
2025-05-28 23:15:39 +0200

    Make sure e2e tests run after XML payload support

--- bri-3/test/sriUseCase.e2e-spec.ts
+++ bri-3/test/sriUseCase.e2e-spec.ts
@@ -21,6 +21,7 @@ import {
   createEddsaSignature,
 } from '../src/shared/testing/utils';
 import {
+  PayloadFormatType,
   WorkstepConfig,
   WorkstepType,
 } from '../src/bri/workgroup/worksteps/models/workstep';
@@ -147,6 +148,7 @@ describe('SRI use-case end-to-end test', () => {
         executionParams: {
           verifierContractAddress: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
         },
+        payloadFormatType: PayloadFormatType.JSON
       },
     );
 
@@ -158,6 +160,7 @@ describe('SRI use-case end-to-end test', () => {
         executionParams: {
           verifierContractAddress: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
         },
+        payloadFormatType: PayloadFormatType.JSON
       },
     );
 
@@ -169,6 +172,7 @@ describe('SRI use-case end-to-end test', () => {
         executionParams: {
           verifierContractAddress: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
         },
+        payloadFormatType: PayloadFormatType.JSON
       },
     );
 
