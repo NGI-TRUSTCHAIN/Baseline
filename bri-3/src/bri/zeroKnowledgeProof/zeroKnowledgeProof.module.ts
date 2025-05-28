9507ff01ed3ee9a36ed61d69403a622539ffebba
biscuitdey <biswashreedey@gmail.com>
2025-05-28 11:09:07 +0530

    Update file path

--- bri-3/src/bri/zeroKnowledgeProof/zeroKnowledgeProof.module.ts
+++ bri-3/src/bri/zeroKnowledgeProof/zeroKnowledgeProof.module.ts
@@ -3,7 +3,7 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { LoggingModule } from '../../shared/logging/logging.module';
 import { CircuitInputsParserService } from './services/circuit/circuitInputsParser/circuitInputParser.service';
 import { SnarkjsCircuitService } from './services/circuit/snarkjs/snarkjs.service';
-import { GeneralCircuitInputsParserService } from './services/circuit/circuitInputsParser/generalCircuitInputParser.service';
+import { GeneralCircuitInputsParserService } from './services/circuit/circuitInputsParser/utils/generalCircuitInputParser.service';
 
 @Module({
   imports: [CqrsModule, LoggingModule],
