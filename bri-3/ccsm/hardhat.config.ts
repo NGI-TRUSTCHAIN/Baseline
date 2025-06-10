dd99ba60b6c476f856bdb6ba3c5ca9c3581faa6e
biscuitdey <biswashreedey@gmail.com>
2025-06-10 11:38:37 +0530

    Remove old contracts

--- bri-3/ccsm/hardhat.config.ts
+++ bri-3/ccsm/hardhat.config.ts
@@ -2,11 +2,11 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
-  defaultNetwork: "localhost",
+  solidity: '0.8.24',
+  defaultNetwork: 'localhost',
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545",
+      url: 'http://127.0.0.1:8545',
     },
     sepolia: {
       url:
