ce178792688e2358831fffbf438053be50ae2a31
biscuitdey <biswashreedey@gmail.com>
2025-08-06 09:55:35 +0530

    Call workstep verifier contract and verify proof onchain

--- bri-3/src/bri/ccsm/services/evm.service.ts
+++ bri-3/src/bri/ccsm/services/evm.service.ts
@@ -8,7 +8,9 @@ import { ICcsmService } from './ccsm.interface';
 import * as fs from 'fs';
 import { ChainProviderFactory } from './chain.provider.factory';
 import { ChainConfig } from '../models/chain.config';
+import { buildBn128, buildBls12381, utils } from 'ffjavascript';
 
+const { unstringifyBigInts } = utils;
 @Injectable()
 export class EvmService implements ICcsmService {
   private provider: Provider;
@@ -73,30 +75,13 @@ export class EvmService implements ICcsmService {
       this.wallet,
     );
 
-    const proofElements = [
-      ...witness.proof.value['A'].slice(0, 2),
-      ...witness.proof.value['B'].slice(0, 2),
-      ...witness.proof.value['C'].slice(0, 2),
-      ...witness.proof.value['Z'].slice(0, 2),
-      ...witness.proof.value['T1'].slice(0, 2),
-      ...witness.proof.value['T2'].slice(0, 2),
-      ...witness.proof.value['T3'].slice(0, 2),
-      ...witness.proof.value['Wxi'].slice(0, 2),
-      ...witness.proof.value['Wxiw'].slice(0, 2),
-      witness.proof.value['eval_a'],
-      witness.proof.value['eval_b'],
-      witness.proof.value['eval_c'],
-      witness.proof.value['eval_s1'],
-      witness.proof.value['eval_s2'],
-      witness.proof.value['eval_zw'],
-    ].map((p) => ethers.toBigInt(p));
-
-    const pubInputs = witness.publicInputs!.map((input) =>
-      ethers.toBigInt(input),
+    const { proofHexBytes, publicInputs } = await this.plonkProofFromWitness(
+      witness.proof.value,
+      witness.publicInputs,
     );
 
     try {
-      return await verifierContract.verifyProof(proofElements, pubInputs);
+      return await verifierContract.verifyProof(proofHexBytes, publicInputs);
     } catch (error) {
       throw new InternalServerErrorException(
         `Error while trying to verify proof on chain : ${error}`,
@@ -125,10 +110,71 @@ export class EvmService implements ICcsmService {
         const bigIntValue = BigInt(value);
         hexValue = '0x' + bigIntValue.toString(16).padStart(64, '0');
       } catch (error) {
-        console.error('Error converting value to BigInt:', value);
-        throw error;
+        throw new Error('Invalid value');
       }
     }
-    return hexValue.slice(2); // Remove '0x' prefix
+    return hexValue; // Remove '0x' prefix
+  }
+
+  private async getCurveFromName(name) {
+    let curve;
+    const normName = this.normalizeName(name);
+    if (['BN128', 'BN254', 'ALTBN128'].indexOf(normName) >= 0) {
+      curve = await buildBn128();
+    } else if (['BLS12381'].indexOf(normName) >= 0) {
+      curve = await buildBls12381();
+    } else {
+      throw new Error(`Curve not supported: ${name}`);
+    }
+    return curve;
+  }
+
+  private normalizeName(n) {
+    return n
+      .toUpperCase()
+      .match(/[A-Za-z0-9]+/g)
+      .join('');
+  }
+
+  private async plonkProofFromWitness(witnessProof, witnessPub) {
+    const proof = unstringifyBigInts(witnessProof);
+    const pub = unstringifyBigInts(witnessPub);
+
+    const curve = await this.getCurveFromName(proof.curve);
+    const G1 = curve.G1;
+    const Fr = curve.Fr;
+
+    const inputs = pub.map((p) => {
+      return this.formatHexString(p);
+    });
+
+    const proofBuff = new Uint8Array(G1.F.n8 * 2 * 9 + Fr.n8 * 7);
+    G1.toRprUncompressed(proofBuff, 0, G1.e(proof.A));
+    G1.toRprUncompressed(proofBuff, G1.F.n8 * 2, G1.e(proof.B));
+    G1.toRprUncompressed(proofBuff, G1.F.n8 * 4, G1.e(proof.C));
+    G1.toRprUncompressed(proofBuff, G1.F.n8 * 6, G1.e(proof.Z));
+    G1.toRprUncompressed(proofBuff, G1.F.n8 * 8, G1.e(proof.T1));
+    G1.toRprUncompressed(proofBuff, G1.F.n8 * 10, G1.e(proof.T2));
+    G1.toRprUncompressed(proofBuff, G1.F.n8 * 12, G1.e(proof.T3));
+    G1.toRprUncompressed(proofBuff, G1.F.n8 * 14, G1.e(proof.Wxi));
+    G1.toRprUncompressed(proofBuff, G1.F.n8 * 16, G1.e(proof.Wxiw));
+    Fr.toRprBE(proofBuff, G1.F.n8 * 18, Fr.e(proof.eval_a));
+    Fr.toRprBE(proofBuff, G1.F.n8 * 18 + Fr.n8, Fr.e(proof.eval_b));
+    Fr.toRprBE(proofBuff, G1.F.n8 * 18 + Fr.n8 * 2, Fr.e(proof.eval_c));
+    Fr.toRprBE(proofBuff, G1.F.n8 * 18 + Fr.n8 * 3, Fr.e(proof.eval_s1));
+    Fr.toRprBE(proofBuff, G1.F.n8 * 18 + Fr.n8 * 4, Fr.e(proof.eval_s2));
+    Fr.toRprBE(proofBuff, G1.F.n8 * 18 + Fr.n8 * 5, Fr.e(proof.eval_zw));
+    Fr.toRprBE(proofBuff, G1.F.n8 * 18 + Fr.n8 * 6, Fr.e(proof.eval_r));
+
+    const proofHex = Array.from(proofBuff).map(this.i2hex).join('');
+
+    return {
+      proofHexBytes: '0x' + proofHex,
+      publicInputs: inputs,
+    };
+  }
+
+  private i2hex(i) {
+    return ('0' + i.toString(16)).slice(-2);
   }
 }
